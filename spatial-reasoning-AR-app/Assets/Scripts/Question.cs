35155938a0e971d138174b78329e03000e1a31b9
Selena Qian <sq22@duke.edu>
2020-09-15 19:43:00 -0400

    created printRotation for testing, Question class, ImageManager to handle changing prefab when question changes

---
+++ spatial-reasoning-AR-app/Assets/Scripts/Question.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{
+    public const float RANGE = 10.0f;
+    private GameObject model;
+    private Vector3 correctRotation;
+    private GameObject[] alternateObjects;
+
+    public Question(String objName, Vector3 vec) {
+      model = (GameObject)Resources.Load(objName);
+      correctRotation = vec;
+    }
+
+    public Question(String objName, Vector3 vec, GameObject[] alts) {
+      model = (GameObject)Resources.Load(objName);
+      correctRotation = vec;
+      alternateObjects = alts;
+    }
+
+    public Boolean isCorrect(int x, int y, int z) {
+      return withinRange(x, correctRotation.x) && withinRange(y, correctRotation.y) && withinRange(z, correctRotation.z);
+    }
+
+    private Boolean withinRange(float current, float desired) {
+      return Math.Abs(current - desired) < RANGE;
+    }
+
+    public GameObject getModel() {
+      return model;
+    }
+}
---
+++ spatial-reasoning-AR-app/Assets/Scripts/printRotation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class printRotation : MonoBehaviour
+{
+  Quaternion previousRotation;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.rotation != previousRotation) {
+          Debug.Log(transform.rotation.eulerAngles);
+          previousRotation = transform.rotation;
+        }
+    }
+}
